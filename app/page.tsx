70cf0f916a0a9c4b77f5926b43b6803cae90fa0c
HRegniez <henriregniez124@gmail.com>
2025-06-05 06:44:52 -0700

    hero part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionsList from '@/components/CompanionsList'
+import CompanionCard from '@/components/ui/CompanionCard'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
     <div>
-      <h1 className='text-2xl'>Hello World</h1>
-      <Button>Click me</Button>
+      <h1 >Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+          id="2"
+          name="Artifika"
+          topic="Learn React"
+          subject="React"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="3"
+          name="Jane Doe"
+          topic="Learn Testing"
+          subject="Testing"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="1"
+          name="John Doe"
+          topic="How to learn AI"
+          subject="AI"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
     </div>
   )
 }
